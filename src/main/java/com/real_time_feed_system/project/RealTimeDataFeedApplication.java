9aec8e62ee23d3a00bf534627e88fe1be2a3e445
amrathmd <amrathmd.mohammed@gmail.com>
2024-09-14 12:53:32 +0530

    Initialize :[Project] Real time data feed system using spring

---
+++ src/main/java/com/real_time_feed_system/project/RealTimeDataFeedApplication.java
@@ -0,0 +1,13 @@
+package com.real_time_feed_system.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeDataFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeDataFeedApplication.class, args);
+	}
+
+}
